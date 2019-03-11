9f647e6db05ad9071c95cc7fe3cc974f02765035
Pradeep Nalabalapu <pradeep.nalabalapu@mbp-e805.local>
2019-03-11 14:58:30 -0500

    printTable info and printDocuments working

--- src/main/java/maprdbConnect/MovieInfo.java
+++ src/main/java/maprdbConnect/MovieInfo.java
@@ -1,17 +1,23 @@
 package maprdbConnect;
 
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+
 import org.ojai.Document;
 import org.ojai.DocumentStream;
 import org.ojai.store.Connection;
-import org.ojai.store.DocumentStore;
-import org.ojai.store.DriverManager;
+
+import com.mapr.db.MapRDB;
+import com.mapr.db.Table;
+import com.mapr.db.exceptions.DBException;
 
 public class MovieInfo {
-	final Connection connection;
 	MovieInfo() {
-		Connection connection = DriverManager.getConnection("ojai:mapr://10.20.30.66:5678?auth=basic;user=mapr;password=mapr;ssl=true;sslCA=/opt/mapr/conf/ssl_truststore.pem;sslTargetNameOverride=psnode66.ps.lab;");
+		//Connection connection = DriverManager.getConnection("ojai:mapr:10.20.30.66:5678?auth=basic;user=mapr;password=mapr;ssl=true;sslCA=/opt/mapr/conf/ssl_truststore.pem;sslTargetNameOverride=psnode66.ps.lab;");
 		//Connection connection = DriverManager.getConnection("ojai:mapr:");
+		/*
 		this.connection = connection;
 		final DocumentStore store = connection.getStore("maprdb_tables/movies");
 
@@ -30,7 +36,40 @@ public class MovieInfo {
 	    connection.close();
 
 	    System.out.println("==== End Application ===");
+	    */
 	}
+	Table table;
+	private Table getTable(String tableName) throws DBException {
+	    
+		if (MapRDB.tableExists(tableName)) {
+			table = MapRDB.getTable(tableName);
+		}
+		return table;
+	}
+	
+	public void printTableInformation(String tableName) throws IOException {
+	    Table table = MapRDB.getTable(tableName);
+	    System.out.println("\n=============== TABLE INFO ===============");
+	    System.out.println(" Table Name : " + table.getName());
+	    System.out.println(" Table Path : " + table.getPath());
+	    System.out.println(" Table Infos : " + Arrays.toString(table.getTabletInfos()));
+	    System.out.println("==========================================\n");
+	  }
+	
+	public void printDocuments(String tableName) throws IOException {
+		Table table = MapRDB.getTable(tableName);
+		System.out.println("Printing all records");
+		DocumentStream rs = table.find();
+		Iterator<Document> itrs = rs.iterator();
+		Document readRecord;
+		while (itrs.hasNext()) {
+			readRecord = itrs.next();
+			System.out.println("\t" + readRecord);
+		}
+		rs.close();
+	}
+
+	
 
 }
 
--- src/test/java/maprdbConnect/DbTest.java
+++ src/test/java/maprdbConnect/DbTest.java
@@ -1,11 +1,27 @@
 package maprdbConnect;
 
+import java.io.IOException;
+
 public class DbTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MovieInfo movieInfo = new MovieInfo();
-		System.out.println("Done");
+		try {
+			movieInfo.printTableInformation("maprdb_tables/movies");
+		} catch (IOException e) {
+			System.out.println("Error "+e);
+		}
+		System.out.println("Done tableinfo");
+		try {
+			movieInfo.printDocuments("maprdb_tables/movies");
+		} catch (IOException e) {
+			System.out.println("Error "+e);
+		}
+		System.out.println("Done tableinfo");
+		
 	}
 
 }
+
+	
